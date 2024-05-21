e61059d726ff84365b7ec08e6c59ccdf1bf2113b
seochanmi <tjcksal627@naver.com>
2024-05-21 10:11:08 +0900

    현재까지 수업했던 html,javascript 예제 추가함

---
+++ javascript/test.js
@@ -0,0 +1,16 @@
+
+/*
+   여기는 test.js파일입니다
+
+   javascript 를 작성하는 파일입니다.
+
+   따라서 javascript 문법에 맞게 작성을 해야 합니다.
+*/
+
+console.log("test.js 파일이 로딩됩니다");
+
+let myName="김구라";
+
+let sing= function(){
+    alert("노래를 불러요!");
+};
---
+++ javascript/test02.js
@@ -0,0 +1,19 @@
+//  test02.js
+/*
+    html 요소에 일어나는 이벤트 종류   정해진 이벤트명을 괄호안에 쓰면 된다
+
+    클릭하면 => click
+    마우스를 커서를 올리면 => mouseover
+    마우스 커서가 요소를 벗어나면 => mouseout
+    마우스를 클릭하면 => mousedown
+    마우스를 클릭후 때면 => mouseup
+    포커스가 오면 => focus
+    포커스를 잃으면 => blur ... 등등
+*/
+// document.querySelector("선택자").addEventListener(이벤트명,콜백함수);   화살표함수: ()=>{}
+// document.querySelector("button").addEventListener("click", function(){});
+// document.querySelector("#myBtn").addEventListener("click", function(){});
+// document.querySelector("#one").addEventListener("mouseover", ()=>{});
+// document.querySelector("#inputMsg").value="안녕하세요";
+// document.querySelector("#one").addEventListener.innerText="p1 입니다";
+
